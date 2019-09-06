61fa8e7a150440fccd79d9e10819b94064d500b2
Faresis <nikignatiev@gmail.com>
2019-09-05 19:57:09 -0700

    Add palindrome permutation check sort implementation.

--- arraysandstrings/PalindromePermutationString.java
+++ arraysandstrings/PalindromePermutationString.java
@@ -4,6 +4,7 @@ import java.util.stream.*;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.joining;
 
 class PalindromePermutationString {
   /**
@@ -19,6 +20,32 @@ class PalindromePermutationString {
       }
     }
     return true;
+  }
+
+  /**
+   * Sort based solution.
+   */
+  static boolean sortPalindromePermutation(String s) {
+    if (s.length() < 2) return true;
+
+    char[] sorted = s.toLowerCase().replaceAll(" ", "").toCharArray();
+    Arrays.sort(sorted);
+    int count = 1;
+    char current = sorted[0];
+    boolean firstOddFound = false;
+    for (int i = 1; i < sorted.length; i++) {
+      if (sorted[i] == current) {
+        count++;
+      } else {
+        if (count % 2 != 0) {
+          if (firstOddFound) return false;
+          else firstOddFound = true;
+        }
+        count = 1;
+        current = sorted[i];
+      }
+    }
+    return count % 2 == 0 || !firstOddFound;
   }
 
   private static Map<Character, Long> countCharacters(String s) {
@@ -43,6 +70,14 @@ class PalindromePermutationString {
     System.out.printf("Map: %s = %b\n", aaxxaa, mapPalindromePermutation(aaxxaa));
     System.out.printf("Map: %s = %b\n", aacxaa, mapPalindromePermutation(aacxaa));
     System.out.printf("Map: %s = %b\n", abcxaa, mapPalindromePermutation(abcxaa));
+
+    System.out.println();
+    System.out.printf("Sort: %s = %b\n", tactcoa, sortPalindromePermutation(tactcoa));
+    System.out.printf("Sort: %s = %b\n", aaaaa, sortPalindromePermutation(aaaaa));
+    System.out.printf("Sort: %s = %b\n", aaxaa, sortPalindromePermutation(aaxaa));
+    System.out.printf("Sort: %s = %b\n", aaxxaa, sortPalindromePermutation(aaxxaa));
+    System.out.printf("Sort: %s = %b\n", aacxaa, sortPalindromePermutation(aacxaa));
+    System.out.printf("Sort: %s = %b\n", abcxaa, sortPalindromePermutation(abcxaa));
   }
 }
 
