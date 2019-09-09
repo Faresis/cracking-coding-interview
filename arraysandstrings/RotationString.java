1776bcf9a4828aefb7fdb201dd8a366886825c56
Faresis <nikignatiev@gmail.com>
2019-09-09 09:07:50 -0700

    Add rotation string implementation.

---
+++ arraysandstrings/RotationString.java
@@ -0,0 +1,34 @@
+class RotationString {
+  static boolean isRotation(String s1, String s2) {
+    if (s1.length() != s2.length()) return false;
+    int s1Idx = s1.length() - 1;
+    int s2Idx = s2.length() - 1;
+    while (s1Idx > 0) {
+      if (s1.charAt(s1Idx) == s2.charAt(s2Idx)) {
+        --s2Idx; --s1Idx;
+      } else {
+        if (s2Idx == s2.length() - 1) {
+          --s1Idx;
+        } else {
+          ++s2Idx;
+        }
+      }
+    }
+    if (s2Idx == 1) return true;
+    return s2.substring(0, s2Idx).equals(s1.substring(s1.length() - s2Idx));
+  }
+
+  public static void main(String[] args) {
+    String s1 = "waterbottle";
+    String s2 = "erbottlewat";
+    System.out.printf("%s : %s -> %s\n", s1, s2, isRotation(s1, s2));
+
+    s1 = "taxat";
+    s2 = "xatta";
+    System.out.printf("%s : %s -> %s\n", s1, s2, isRotation(s1, s2));
+
+    s1 = "taxar";
+    s2 = "xatra";
+    System.out.printf("%s : %s -> %s\n", s1, s2, isRotation(s1, s2));
+  }
+}
