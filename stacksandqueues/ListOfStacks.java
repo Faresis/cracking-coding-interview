9cd204627491445d8d9567911d66ea16be99ca04
Faresis <nikignatiev@gmail.com>
2020-01-09 18:05:32 -0800

    3.3. List of Stacks. Pop at index fixed.

--- stacksandqueues/ListOfStacks.java
+++ stacksandqueues/ListOfStacks.java
@@ -48,7 +48,10 @@ public class ListOfStacks {
 
   public int popAt(int idx) {
     if (stacks.size() <= idx) throw new ArrayIndexOutOfBoundsException();
-    return stacks.get(idx).pop();
+    Stack stack = stacks.get(idx);
+    int result = stack.pop();
+    if (stack.isEmpty()) stacks.remove(idx);
+    return result;
   }
 
   public void print() {
@@ -114,23 +117,36 @@ public class ListOfStacks {
 
   public static void main(String[] args) {
     ListOfStacks ls = new ListOfStacks(3);
-    ls.push(1);
-    ls.push(2);
-    ls.push(3);
-    ls.push(4);
-    ls.push(5);
-    ls.push(6);
-    ls.push(7);
-    ls.push(8);
-    ls.push(9);
-    ls.push(10);
 
+    System.out.println("Full stack.");
+    add(ls, 10);
     ls.print();
 
+    System.out.println("Clearing stack.");
     while (!ls.isEmpty())
       System.out.println(ls.pop());
-
     ls.print();
+
+    System.out.println("Full stack.");
+    add(ls, 10);
+    ls.print();
+
+
+    System.out.println("Removing from the middle.");
+    System.out.println(ls.popAt(1));
+    System.out.println(ls.popAt(1));
+    System.out.println(ls.popAt(1));
+    ls.print();
+
+    System.out.println("Clearing stack.");
+    while (!ls.isEmpty())
+      System.out.println(ls.pop());
+    ls.print();
+  }
+
+  private static void add(ListOfStacks ls, int num) {
+    for (int i = 1; i <= num; i++)
+      ls.push(i);
   }
 }
 
