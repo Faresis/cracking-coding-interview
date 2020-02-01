9816d1d67b1315697d0658d3488eeb0ec32a5ac8
Faresis <nikignatiev@gmail.com>
2020-01-31 17:25:15 -0800

    4.5. Is binary search tree.

---
+++ graphs/IsBinarySearchTree.java
@@ -0,0 +1,90 @@
+import java.util.Arrays;
+
+public class IsBinarySearchTree {
+  private static class Tree {
+    Node root;
+
+    Tree(Node root) { this.root = root; }
+
+    @Override public String toString() { return this.root.toString(); }
+
+    public static Tree toTree(int[] arr) {
+      return new Tree(toTree(arr, 0, arr.length));
+    }
+
+    private static Node toTree(int[] arr, int start, int end) {
+      if (start == end) return null;
+
+      int mid = (end - start)/2 + start;
+      Node curr = new Node(arr[mid]);
+      curr.left = toTree(arr, start, mid);
+      curr.right = toTree(arr, mid+1, end);
+      return curr;
+    }
+  }
+
+  private static class Node {
+    int data;
+    int lvl; // for toString only
+    Node left;
+    Node right;
+
+    Node(int data) { this.data = data; }
+
+    @Override public String toString() {
+      String result = "";
+      if (this.left != null) {
+        this.left.lvl = this.lvl + 1;
+        result += this.left.toString();
+      }
+      result += " Level: " + this.lvl + " Data: "  + this.data;
+      if (this.right != null) {
+        this.right.lvl = this.lvl + 1;
+        result += this.right.toString();
+      }
+      return result;
+    }
+  }
+
+  public static boolean isBst(Tree tree) {
+    return isBst(tree.root);
+  }
+
+  private static boolean isBst(Node node) {
+    if (node == null) return true;
+    boolean left = isBst(node.left);
+    if (!left) return left;
+    boolean right = isBst(node.right);
+    if (!right) return right;
+    boolean result = true;
+    if (node.left != null) result &= node.left.data < node.data;
+    if (node.right != null) result &= node.right.data > node.data;
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {10, 24, 31, 42, 57, 63, 79, 82, 98, 105};
+
+    Tree tree = Tree.toTree(arr);
+    System.out.println("Array: " + Arrays.toString(arr));
+    System.out.println("Tree: " + tree);
+    System.out.println("Is bst: " + isBst(tree));
+
+    tree.root.left.left.data = 70;
+
+    System.out.println("Tree: " + tree);
+    System.out.println("Is bst: " + isBst(tree));
+
+    tree.root.left.left.data = 24;
+    tree.root.right.data = 62;
+
+    System.out.println("Tree: " + tree);
+    System.out.println("Is bst: " + isBst(tree));
+
+    tree.root.right.data = 83;
+
+    System.out.println("Tree: " + tree);
+    System.out.println("Is bst: " + isBst(tree));
+  }
+}
+
