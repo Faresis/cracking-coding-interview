a1339e9776180d85a4f03d669a00aa07f21e7d58
Faresis <nikignatiev@gmail.com>
2020-02-25 17:59:55 -0800

    5.1. Insertion. Revised.

--- bits/Insertion.java
+++ bits/Insertion.java
@@ -1,10 +1,19 @@
 public class Insertion {
   public static void main(String[] args) {
-    System.out.println(insert(1024, 19, 2, 6));
+    System.out.println(insert(
+                              1024, // 10000000000
+                              19,   // 10011
+                              2,
+                              6));
   }
 
   public static int insert(int n, int m, int i, int j) {
-    int mask = (-1 << j) | ((1 << i) - 1);
+    // 1s0000000 OR 0s11 = 1s0000011
+    int mask = (-1 << ++j) | ((1 << i) - 1);
+
+    // clear bits i to j in n
+    // then shift m to i bits left
+    // set m bits in n from i to j with OR
     return (n & mask) | (m << i);
   }
 }
