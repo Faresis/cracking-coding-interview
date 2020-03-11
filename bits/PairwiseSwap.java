b5755b7f8c046ac1ec7345b6af7b2db095c7aa11
Faresis <nikignatiev@gmail.com>
2020-03-10 17:26:31 -0700

    5.7. Pairwise swap. Revised after hints.

--- bits/PairwiseSwap.java
+++ bits/PairwiseSwap.java
@@ -6,6 +6,13 @@ public class PairwiseSwap {
     System.out.println("Swap 0: " + swap(0));
     System.out.println("Swap 1: " + swap(1));
     System.out.println("Swap ~0: " + swap(~0));
+
+    System.out.println("Swap revised 1010101010: " + swapRevised(0b1010101010));
+    System.out.println("Swap revised 1010111110: " + swapRevised(0b1010111110));
+    System.out.println("Swap revised 1010000000: " + swapRevised(0b1010000000));
+    System.out.println("Swap revised 0: " + swapRevised(0));
+    System.out.println("Swap revised 1: " + swapRevised(1));
+    System.out.println("Swap revised ~0: " + swapRevised(~0));
   }
 
   public static int swap(int num) {
@@ -17,6 +24,10 @@ public class PairwiseSwap {
       mask <<= 2;
     }
     return num;
+  }
+
+  public static int swapRevised(int num) {
+    return ((num & 0xaaaaaaaa) >> 1) | ((num & (~0xaaaaaaaa)) << 1);
   }
 }
 
