23a2fcf6668c791985110cdafa950c78e8db41a8
Faresis <nikignatiev@gmail.com>
2020-03-23 08:20:50 -0700

    5.8. Draw a line. Revised implementation.

--- bits/DrawLine.java
+++ bits/DrawLine.java
@@ -12,10 +12,22 @@ public class DrawLine {
     drawLine(screen, width, 3, 3, 5);
     print(screen, width);
 
+    System.out.println("Single pixel scenario. Revised.");
+    screen = new byte[30];
+    print(screen, width); // clear screen
+    drawLineRevised(screen, width, 3, 3, 5);
+    print(screen, width);
+
     System.out.println("Middle full pixel scenario.");
     screen = new byte[30];
     print(screen, width);
     drawLine(screen, width, 3, 17, 7);
+    print(screen, width);
+
+    System.out.println("Middle full pixel scenario. Revised.");
+    screen = new byte[30];
+    print(screen, width);
+    drawLineRevised(screen, width, 3, 17, 7);
     print(screen, width);
   }
 
@@ -28,6 +40,37 @@ public class DrawLine {
       System.out.print(String.format("%8s", Integer.toBinaryString(screen[i] & 0xFF)).replace(' ', '0'));
     }
     System.out.println("\nScreen end: ");
+  }
+
+  private static void drawLineRevised(byte[] screen, int width, int x1, int x2, int y) {
+    int start_offset = x1 % 8;
+    int first_full_byte = x1 / 8;
+    if (start_offset != 0) first_full_byte++;
+
+    int end_offset = x2 % 8;
+    int last_full_byte = x2 / 8;
+    if (end_offset != 7) last_full_byte--;
+
+    for (int i = first_full_byte; i <= last_full_byte; i++) {
+      screen[width/8*y+i] = (byte) 0xFF;
+    }
+
+    byte start_mask = (byte) (0xFF >> start_offset);
+    byte end_mask = (byte) ~(0xFF >> (end_offset + 1));
+
+    if ((x1/8) == (x2/8)) {
+      byte mask = (byte) (start_mask & end_mask);
+      screen[width/8*y + x1/8] |= mask;
+    } else {
+      if (start_offset != 0) {
+        int byteIdx = width/8*y + first_full_byte - 1;
+        screen[byteIdx] |= start_mask;
+      }
+      if (end_offset != 7) {
+        int byte_number = width/8*y + last_full_byte + 1;
+        screen[byte_number] |= end_mask;
+      }
+    }
   }
 
   private static void drawLine(byte[] screen, int width, int x1, int x2, int y) {
