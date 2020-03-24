627d401f87a256b783ac08df8396111ece28e828
Faresis <nikignatiev@gmail.com>
2020-03-23 17:47:24 -0700

    6. Primes. Slightly improved.

--- math/Primes.java
+++ math/Primes.java
@@ -3,12 +3,22 @@ public class Primes {
     System.out.println("Is Prime naive.");
     for (int i = 1; i < 20; i++)
       System.out.println(i + ": " + isPrimeNaive(i));
+
+    System.out.println("Is Prime improved.");
+    for (int i = 1; i < 20; i++)
+      System.out.println(i + ": " + isPrimeImproved(i));
   }
 
   private static boolean isPrimeNaive(int n) {
     for (int i = 2; i < n; i++) {
       if (n % i == 0) return false;
     }
+    return true;
+  }
+
+  private static boolean isPrimeImproved(int n) {
+    for (int i = 2; i*i <= n; i++)
+      if (n % i == 0) return false;
     return true;
   }
 }
