8a6d6c3969fe296c42ed9e1e0387c5a67f716660
Faresis <nikignatiev@gmail.com>
2020-03-24 08:33:45 -0700

    6. Primes. Sieve of Eratosthenes.

--- math/Primes.java
+++ math/Primes.java
@@ -1,12 +1,17 @@
 public class Primes {
   public static void main(String[] args) {
     System.out.println("Is Prime naive.");
-    for (int i = 1; i < 20; i++)
+    for (int i = 2; i < 20; i++)
       System.out.println(i + ": " + isPrimeNaive(i));
 
     System.out.println("Is Prime improved.");
-    for (int i = 1; i < 20; i++)
+    for (int i = 2; i < 20; i++)
       System.out.println(i + ": " + isPrimeImproved(i));
+
+    System.out.println("Sieve of Eratosthenes.");
+    boolean[] primes = primes(20);
+    for (int i = 2; i < primes.length; i++)
+      System.out.println(i + ": " + primes[i]);
   }
 
   private static boolean isPrimeNaive(int n) {
@@ -20,6 +25,31 @@ public class Primes {
     for (int i = 2; i*i <= n; i++)
       if (n % i == 0) return false;
     return true;
+  }
+
+  private static boolean[] primes(int max) {
+    boolean[] primes = new boolean[max + 1];
+    for (int i = 2; i < primes.length; i++)
+      primes[i] = true;
+
+    int prime = 2;
+    while (prime*prime <= max) {
+      crossOff(primes, prime);
+      prime = nextPrime(primes, prime);
+    }
+    return primes;
+  }
+
+  private static void crossOff(boolean[] primes, int prime) {
+    for (int i = prime*prime; i < primes.length; i += prime)
+      primes[i] = false;
+  }
+
+  private static int nextPrime(boolean[] primes, int prime) {
+    int next = prime + 1;
+    while (next < primes.length && !primes[next])
+      ++next;
+    return next;
   }
 }
 
