145ce7af6a0d95ed2e881211761f2518e36c4d06
Faresis <nikignatiev@gmail.com>
2020-04-13 09:21:02 -0700

    6.7. Girls ratio. Revised.

--- math/GirlsRatio.java
+++ math/GirlsRatio.java
@@ -2,23 +2,33 @@ import java.util.Random;
 
 public class GirlsRatio {
   static Random gen = new Random();
-  static double ratio() {
-    double b = 0d, g = 0d;
-    do {
-      if (gen.nextInt(2) == 0) 
+
+  static int[] family() {
+    int b = 0, g = 0;
+    while (g == 0) {
+      if (gen.nextBoolean())
         b++;
       else
         g++;
-    } while (g < 1);
-    return g/(b+g);
+    }
+    return new int[] {b, g};
   }
 
   public static void main(String[] args) {
     double ratios = 0d;
-    int families = 1_000_000;
-    for (int i = 0; i < families; i++)
-      ratios += ratio();
-    System.out.println(ratios/families);
+    int families = 1_000_000_000;
+    int boys = 0, girls = 0;
+    double familyRatio = 0d;
+    for (int i = 0; i < families; i++) {
+      int[] res = family();
+      boys += res[0];
+      girls += res[1];
+      familyRatio += res[1]/(double)(res[0]+res[1]);
+    }
+    System.out.println("Average family ratio: " + familyRatio/families);
+    System.out.println("Boys: " + boys);
+    System.out.println("Girls: " + girls);
+    System.out.println("Global Ratio g/(b+g): " + girls/(double)(boys+girls));
   }
 }
 
