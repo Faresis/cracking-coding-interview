adffae799689978414efaf89627d433978fc95b7
Faresis <nikignatiev@gmail.com>
2020-06-05 07:51:22 -0700

    Fibonacci sequence. Memoization.

--- dynamic/FibonacciMemo.java
+++ dynamic/FibonacciMemo.java
@@ -1,9 +1,14 @@
-public class Fibonacci {
-  public static int fib(int n) {
+public class FibonacciMemo {
+  public static long fib(int n) {
+    return fib(n, new long[n+1]);
+  }
+
+  public static long fib(int n, long[] memo) {
     if (n < 0) throw new IllegalArgumentException();
     if (n == 0) return 0;
     if (n == 1) return 1;
-    return fib(n - 1) + fib(n - 2);
+    if (memo[n] == 0) memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n];
   }
 
   public static void main(String[] args) {
