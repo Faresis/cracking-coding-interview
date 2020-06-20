867b11b479b1dbe8737a6b5d7a1ce404e9876e20
Faresis <nikignatiev@gmail.com>
2020-06-19 18:37:55 -0700

    8.5. Multiplication. V3.

--- dynamic/Multiplication.java
+++ dynamic/Multiplication.java
@@ -21,6 +21,23 @@ public class Multiplication {
     System.out.println("7*8:" + multiply_v2(7, 8));
     System.out.println("1024*1024:" + multiply_v2(1024, 1024));
     System.out.println("Calls v2: " + calls_v2);
+    System.out.println("9456*18741:" + multiply_v2(9456, 18741));
+    System.out.println("Calls v2: " + calls_v2);
+
+    System.out.println("V3:");
+    System.out.println("6*0:" + multiply_v3(6, 0));
+    System.out.println("0*5:" + multiply_v3(0, 5));
+    System.out.println("1*5:" + multiply_v3(1, 5));
+    System.out.println("6*1:" + multiply_v3(6, 1));
+    System.out.println("6*5:" + multiply_v3(6, 5));
+    System.out.println("6*6:" + multiply_v3(6, 6));
+    System.out.println("7*8:" + multiply_v3(7, 8));
+    calls_v3 = 0;
+    System.out.println("1024*1024:" + multiply_v3(1024, 1024));
+    System.out.println("Calls v3: " + calls_v3);
+    calls_v3 = 0;
+    System.out.println("9456*18741:" + multiply_v3(9456, 18741));
+    System.out.println("Calls v3: " + calls_v3);
   }
 
   static long calls_v1 = 0;
@@ -50,6 +67,17 @@ public class Multiplication {
     if (b == 1) return a - diff;
     if (b % 2 == 0) return multiply_v2_rec(a << 1, b >> 1, diff);
     else return multiply_v2_rec(a + a, (b+1) >> 1, diff + a);
+  }
+
+  static long calls_v3 = 0;
+  static long multiply_v3(int a, int b) {
+    calls_v3++;
+    if (a == 0 || b == 0) return 0;
+    if (a == 1) return b;
+    if (b == 1) return a;
+    if (a % 2 == 0) return multiply_v3(a >> 1, b) << 1;
+    if (b % 2 == 0) return multiply_v3(a, b >> 1) << 1;
+    return (multiply_v3(a >> 1, b) << 1) + b;
   }
 }
 
