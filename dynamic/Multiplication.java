077aeda2a21f56125b99541ad2e925216c39ef59
Faresis <nikignatiev@gmail.com>
2020-06-19 17:57:19 -0700

    8.5. Multiplication. V2.

--- dynamic/Multiplication.java
+++ dynamic/Multiplication.java
@@ -1,8 +1,22 @@
 public class Multiplication {
   public static void main(String[] args) {
     System.out.println("V1:");
+    System.out.println("6*0:" + multiply_v1(6, 0));
+    System.out.println("0*5:" + multiply_v1(0, 5));
+    System.out.println("1*5:" + multiply_v1(1, 5));
+    System.out.println("6*1:" + multiply_v1(6, 1));
     System.out.println("6*5:" + multiply_v1(6, 5));
+    System.out.println("6*6:" + multiply_v1(6, 6));
     System.out.println("7*8:" + multiply_v1(7, 8));
+
+    System.out.println("V2:");
+    System.out.println("6*0:" + multiply_v2(6, 0));
+    System.out.println("0*5:" + multiply_v2(0, 5));
+    System.out.println("1*5:" + multiply_v2(1, 5));
+    System.out.println("6*1:" + multiply_v2(6, 1));
+    System.out.println("6*5:" + multiply_v2(6, 5));
+    System.out.println("6*6:" + multiply_v2(6, 6));
+    System.out.println("7*8:" + multiply_v2(7, 8));
   }
 
   static long multiply_v1(int a, int b) {
@@ -15,6 +29,17 @@ public class Multiplication {
   private static long multiply_v1_rec(int a, int b, long result) {
     if (b == 0) return result;
     return multiply_v1_rec(a, --b, result+a);
+  }
+
+  static long multiply_v2(int a, int b) {
+    return multiply_v2_rec(a, b, 0);
+  }
+
+  private static long multiply_v2_rec(long a, int b, long diff) {
+    if (b == 0) return 0;
+    if (b == 1) return a - diff;
+    if (b % 2 == 0) return multiply_v2_rec(a << 1, b >> 1, diff);
+    else return multiply_v2_rec(a + a, (b+1) >> 1, diff + a);
   }
 }
 
